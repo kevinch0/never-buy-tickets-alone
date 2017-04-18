e6c1e398c08e777e0a338b076d87d1787a4c8cc5
Mac <taralgurung@hotmail.com>
2017-04-18 15:06:46 +0800

    index page all functionality added and working

--- app.rb
+++ app.rb
@@ -81,3 +81,69 @@ delete("/artist/:id") do
   artist_to_be_deleted.destroy()
   redirect("/")
 end
+
+post ('/venue') do
+  name = params.fetch('name')
+  address = params.fetch('address')
+  imageurl = params.fetch('imageurl')
+  venue = Venue.create({:name => name, :address => address, :imageurl => imageurl})
+  if venue.save()
+    redirect ('/')
+  else
+    erb(:errors)
+  end
+end
+
+get('/venue/:id') do
+  @venue=Venue.find(Integer(params.fetch('id')))
+  erb(:venue)
+end
+
+patch("/venue/:id") do
+  venue_id=Integer(params.fetch('id'))
+  name = params.fetch("name")
+  address = params.fetch('address')
+  imageurl = params.fetch('imageurl')
+  @venue = Venue.find(params.fetch("id").to_i())
+  @venue.update({:name => name, :address => address, :imageurl => imageurl})
+  redirect("/venue/#{venue_id}")
+end
+
+
+delete("/venue/:id") do
+  venue_id=Integer(params.fetch("id"))
+  venue_to_be_deleted= Venue.find(venue_id)
+  venue_to_be_deleted.destroy()
+  redirect("/")
+end
+
+post ('/category') do
+  name = params.fetch('name')
+  category = Category.create({:name => name})
+  if category.save()
+    redirect ('/')
+  else
+    erb(:errors)
+  end
+end
+
+get('/category/:id') do
+  @category=Category.find(Integer(params.fetch('id')))
+  erb(:category)
+end
+
+patch("/category/:id") do
+  category_id=Integer(params.fetch('id'))
+  name = params.fetch("name")
+  @category = Category.find(params.fetch("id").to_i())
+  @category.update({:name => name})
+  redirect("/category/#{category_id}")
+end
+
+
+delete("/category/:id") do
+  category_id=Integer(params.fetch("id"))
+  category_to_be_deleted= Category.find(category_id)
+  category_to_be_deleted.destroy()
+  redirect("/")
+end
